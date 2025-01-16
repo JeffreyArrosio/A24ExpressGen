be2864d911d34299638ba211b0efbdf9b6dd1933
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-01-16 20:51:28 +0000

    A25 para render

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Probando Nodemon V3' });
+  res.render('index', { title: 'Bicicletas' });
 });
 
 module.exports = router;
