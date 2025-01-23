64957af6795e95f2aa8b25fc66c36f0fadc87ac4
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-01-23 21:31:44 +0000

    A26

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bicicletasAPIRouter = require('./routes/api/bicicletas');
 var app = express();
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
 app.use(express.json());
 
 //favicon
@@ -17,7 +19,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/bicicletas', bicicletasAPIRouter); 
+app.use('/api/bicicletas', bicicletasAPIRouter);
 
 // Configuraciones
 app.set('views', path.join(__dirname, 'views'));
@@ -32,15 +34,49 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Bicicletas Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Bicicletas",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/api/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
+
 //Errores
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -49,6 +85,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 
 //Arranque
--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -1,3 +1,121 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Bicicleta:
+ *       type: object
+ *       required:
+ *         - id
+ *         - color
+ *         - modelo
+ *         - ubicacion
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: id de la bici
+ *         color:
+ *           type: string
+ *           description: Color de la bici
+ *         modelo:
+ *           type: string
+ *           description: Modelo de la bici
+ *         longitud:
+ *           type: double
+ *           description: longitud
+ *         latitud:
+ *           type: double
+ *           description: latitud
+ *       example:
+ *         id: 1
+ *         color: rojo
+ *         modelo: Duccati
+ *         longitud: 10.23213
+ *         latitud: 12.3422
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Bicicletas
+ *   description: The Bici managing API
+ * /api/bicicletas:
+ *   get:
+ *     summary: Lists all the Bicicletas
+ *     tags: [Bicicleta]
+ *     responses:
+ *       200:
+ *         description: The list of the Bicicletas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Bicicleta'
+ * /api/bicicletas/create:
+ *   post:
+ *     summary: Create a new Bicicleta
+ *     tags: [Bicicleta]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Bicicleta'
+ *     responses:
+ *       200:
+ *         description: The created Bicicleta.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Bicicleta'
+ *       500:
+ *         description: Some server error
+ * /api/bicicletas/update/{id}:
+ *   put:
+ *    summary: Update the Bicicleta by the id
+ *    tags: [Bicicleta]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: The Bicicleta id
+ *    requestBody:
+ *      required: false
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Bicicleta'
+ *    responses:
+ *      200:
+ *        description: The Bicicleta was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Bicicleta'
+ *      404:
+ *        description: The Bicicleta was not found
+ *      500:
+ *        description: Some error happened
+ * /api/bicicletas/delete:
+ *   delete:
+ *     summary: Remove the Bicicleta by id
+ *     tags: [Bicicleta]
+ *     requestBody:
+ *       required: false
+ *       content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Bicicleta'
+ *     responses:
+ *       200:
+ *         description: The Bicicleta was deleted
+ *       404:
+ *         description: The Bicicleta was not found
+ */
+
+
 let express = require('express')
 let router = express.Router()
 let BicicletaControllerAPI = require("../../controllers/api/BicicletaControllerAPI")
